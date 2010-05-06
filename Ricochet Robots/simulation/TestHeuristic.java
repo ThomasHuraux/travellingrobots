ed3560dc5c40b1522710aee89be7b39ffeeb259f
thomas.huraux <thomas.huraux@c7badcfa-80d2-4a57-37c5-f69f6a14dca6>
2010-05-06 21:01:30 +0000

    TestAndView permet de visualiser separement les problemes irresolus et ceux qui le sont

--- Ricochet Robots/simulation/TestHeuristic.java
+++ Ricochet Robots/simulation/TestHeuristic.java
@@ -10,6 +10,10 @@ public class TestHeuristic {
 	
 	private static final String PATH = "tests/";
 	
+	
+	Sequence unsolved = new Sequence();
+	Sequence solved = new Sequence();
+	
 	Sequence steps = new Sequence();
 	Environment current = new Environment();
 	Heuristic h;
@@ -20,6 +24,8 @@ public class TestHeuristic {
 		AStar.MAXTIME = maxtime;
 		CorridorHeuristic.COSTIMPORTANCE = costImportance;
 		CorridorHeuristic.PRECALCDEPTH = precalcDepth;
+		
+
 		
 		switch(HeuristicID){
 			case Heuristic.CorridorHeuristicID :
@@ -46,24 +52,36 @@ public class TestHeuristic {
     			steps = algo.search(current);
     			float timeH = algo.getTimelength();
     			if(steps == null){
+    				unsolved.add(algo.close.get(0));
     				steps = new Sequence();
     				steps.add(algo.close.get(0));
     				fw.write("-1\t"+AStar.MAXTIME);
-    			}else
+    			}else{
+    				solved.add(steps.get(0));
     				fw.write(algo.current.getCost()+"\t"+timeH);
+    			}
     			fw.write("\t"+((Node)steps.get(0)).getEnvironment().getStates().get(0).getPosition().toString()+"\t");
     			fw.write(((Node)steps.get(0)).getEnvironment().getStates().get(1).getPosition().toString()+"\t");
     			fw.write(((Node)steps.get(0)).getEnvironment().getStates().get(2).getPosition().toString()+"\t");
     			fw.write(((Node)steps.get(0)).getEnvironment().getStates().get(3).getPosition().toString()+"\t");
     			fw.write(current.getTarget().toString()+"\n");
     		}
-    		System.out.println("== END TESTS ==");
+
+    		System.out.println("\n== END TESTS ("+unsolved.size()+" unsolved) ==");
             fw.flush();
             fw.close();
         } 
 		catch (IOException ex) {
             ex.printStackTrace();
         }
+	}
+
+	public Sequence getUnsolved() {
+		return unsolved;
+	}
+
+	public Sequence getSolved() {
+		return solved;
 	}
 
 }
